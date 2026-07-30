36c229b33e1c97f3eda9d03956335b3a34c0301a
SahilSarmalkar99 <sahilsarmalkar99@gmail.com>
2026-07-30 17:18:16 +0530

    added render api-2

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     // baseURL: "http://localhost:5000/api",
-    baseURL: "https://canteen-z071.onrender.com",
+    baseURL: "https://canteen-z071.onrender.com/api",
     withCredentials: true,
 });
 
